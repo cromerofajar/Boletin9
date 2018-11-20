8cffcb3834e258f8219809647299bc62f3ea7add
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-20 10:42:41 +0100

    arreglado

--- Boletin9_6/src/boletin9_6/Empleado.java
+++ Boletin9_6/src/boletin9_6/Empleado.java
@@ -10,18 +10,27 @@ public class Empleado {
     public Empleado(){}
     
     public void trabajador(){
-    int soldo, superior=0, porcentaje=0, soldos=0, trabaj=0,trabajmenor=0;
+    int soldo, superior=0, trabaj=0;
+    float porcentaje=0,trabajmenor=0;
         System.out.println("Introduzca soldo do empleado");
         Scanner ler= new Scanner (System.in);
         soldo=ler.nextInt();
+        while (soldo<0){
+            System.out.println("Introduzca soldo positovo");
+            soldo=ler.nextInt();
+            
+        }
     while (soldo<0 || soldo>0){
         trabaj++;
         if (soldo>=1000 && soldo<=1500){
             superior++;
+            porcentaje=(trabajmenor*100)/trabaj;
         }
-        else{
+        else if (soldo>1500){
+            porcentaje=(trabajmenor*100)/trabaj;
+        }
+        else {
             trabajmenor++;
-            soldos=soldo+soldos;
             porcentaje=(trabajmenor*100)/trabaj;
         }
         soldo=ler.nextInt();
